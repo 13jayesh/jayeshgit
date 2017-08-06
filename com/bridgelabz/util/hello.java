8939a0317590ad2c2b4da982068d560d8dbe59af
jayesh patil <jayeshnpatil13@gmail.com>
2017-08-06 17:02:22 +0530

    commit massage

---
+++ com/bridgelabz/util/hello.java
@@ -0,0 +1,18 @@
+
+/********************
+ Compilation:  javac -d bin hello.java
+ *  Execution:    java -cp bin com.bridgelabz.util.hello n
+ *  Purpose: print hello.
+ *
+ *  @author  BridgeLabz
+ *  @version 1.0
+ *  @since   06-08-2017
+ ******************/
+package com.bridgelabz.util;
+public class hello
+{
+public static void main(String args[])
+{
+System.out.println("hello world");
+}
+}
