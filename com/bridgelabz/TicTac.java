a0cc1e9410462b11d7e73f0e4531d1392e8d4c1b
13jayesh <30770729+13jayesh@users.noreply.github.com>
2017-10-17 23:04:57 +0530

    Add files via upload
    commit

--- com/bridgelabz/TicTac.java
+++ com/bridgelabz/TicTac.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.util;
 import java.util.Scanner;
-import java.util.Random;
 public class TicTac
 	{
+	
+	public static char A[][]=new char[3][3];        
+	public static char  user ='x';
+	public static char computer = 'o';
 
-public static void main(String args[])
+	public static void main(String args[])
 	{
-	
-
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++)
+				A[i][j]=' ';
+		}
 	System.out.println("welcome to tic tac game");
-	Scanner sc=new Scanner(System.in);
-	
-	
-	int row,coloum;
-		if(Math.random()>0.5)
+	if(Math.random()>0.5)
 		{
 		System.out.println("user is 1st");
 		User(A);
@@ -28,18 +29,19 @@ public static void main(String args[])
 
 	}
 
-	public static char A[][]=new char[3][3];        
-	public static char  user ='x';
-	public static char computer = 'o';
 	public static void User(char A[][])
 		{	
 			Scanner sc=new Scanner(System.in);
+			System.out.println("Your Turn:");
 			int row=sc.nextInt();
 			int coloum=sc.nextInt();
-			if(A[row][coloum] !='x' || A[row][coloum] !='o')
+			
+			if(A[row][coloum] !='x' && A[row][coloum] !='o')
 			{
 				A[row][coloum]='x';
 				System.out.println("A["+row+"]["+coloum+"]"+user);
+				System.out.println();
+				printTheBoard(A);
 				if(userwin(A))
 				{
 				System.out.println("user win");	
@@ -47,20 +49,24 @@ public static void main(String args[])
 				else
 					computer(A);
 			}
-			else
+			else{
+				System.out.println("Non Empty Feild");
 				User(A);
-
+			}
+			sc.close();
 		}
 
 	public static void computer(char A[][])
-		{			
+		{	
+			System.out.println("Computer Turn:");
 			int row=(int) (Math.random()*3);
-					
 			int coloum=(int)(Math.random()*3);
-			if(A[row][coloum] !='x' || A[row][coloum] !='o')
+			System.out.println("A["+row+"]["+coloum+"]"+computer);
+			System.out.println();
+			if(A[row][coloum] !='x' && A[row][coloum] !='o')
 			{
-				A[row][coloum]='x';
-				System.out.println("A["+row+"]["+coloum+"]"+computer);
+				A[row][coloum]='o';
+				printTheBoard(A);
 				if(computerwin(A))
 				{
 				System.out.println("computer win");
@@ -68,18 +74,21 @@ public static void main(String args[])
 				else
 					User(A);
 			}
-			else
+			else{
+				System.out.println("Non Empty Feild");
 				computer(A);
+			}
 		}
 
 	public static boolean computerwin(char A[][])
 		{
-		if(A[0][0]=='o' && A[0][1]=='o' && A[0][2]=='o' ||
-		   A[0][0]=='o' && A[1][0]=='o' && A[2][0]=='o' ||
-		   A[0][2]=='o' && A[1][2]=='o' && A[2][2]=='o' ||
-		   A[2][0]=='o' && A[2][1]=='o' && A[2][2]=='o' ||
-		   A[0][0]=='o' && A[1][1]=='o' && A[2][2]=='o' ||
-		   A[0][2]=='o' && A[1][2]=='o' && A[2][0]=='o')
+		if((A[0][0]=='o' && A[0][1]=='o' && A[0][2]=='o') ||
+		   (A[0][0]=='o' && A[1][0]=='o' && A[2][0]=='o') ||
+		   (A[0][2]=='o' && A[1][2]=='o' && A[2][2]=='o') ||
+		   (A[1][0]=='o' && A[1][1]=='o' && A[1][2]=='o') ||
+		   (A[2][0]=='o' && A[2][1]=='o' && A[2][2]=='o') ||
+		   (A[0][0]=='o' && A[1][1]=='o' && A[2][2]=='o') ||
+		   (A[0][2]=='o' && A[1][1]=='o' && A[2][0]=='o'))
 			{
 			return true;
 			}
@@ -89,12 +98,13 @@ public static void main(String args[])
 
 	public static boolean userwin(char A[][])
 		  {
-		 if(A[0][0]=='x' && A[0][1]=='x' && A[0][2]=='x' ||
-		    A[0][0]=='x' && A[1][0]=='x' && A[2][0]=='x' ||
-		    A[0][2]=='x' && A[1][2]=='x' && A[2][2]=='x' ||
-		    A[2][0]=='x' && A[2][1]=='x' && A[2][2]=='x' ||
-		    A[0][0]=='x' && A[1][1]=='x' && A[2][2]=='x' ||
-		    A[0][2]=='x' && A[1][2]=='x' &&A[2][0]=='x') 
+		 if((A[0][0]=='x' && A[0][1]=='x' && A[0][2]=='x') ||
+		    (A[0][0]=='x' && A[1][0]=='x' && A[2][0]=='x') ||
+		    (A[0][2]=='x' && A[1][2]=='x' && A[2][2]=='x') ||
+		    (A[1][0]=='o' && A[1][1]=='o' && A[1][2]=='o') ||
+		    (A[2][0]=='x' && A[2][1]=='x' && A[2][2]=='x') ||
+		    (A[0][0]=='x' && A[1][1]=='x' && A[2][2]=='x') ||
+		    (A[0][2]=='x' && A[1][1]=='x' && A[2][0]=='x')) 
 			{
 			return true;
 			}
@@ -106,10 +116,17 @@ public static void main(String args[])
 
 		}
 
-
+	public static void printTheBoard(char A[][]) {
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				System.out.print("_");
+				System.out.print(A[i][j]+" | ");
+			}	
+			System.out.println();
+		}
+	}
 
 
 
 	
 }
-
